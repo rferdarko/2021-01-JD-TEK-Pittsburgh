ea259e54f293ab22ac025a5883de507457163fe7
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-03-25 15:24:12 -0400

    multiole commits

--- hacker_rank_sql_practice.sql
+++ hacker_rank_sql_practice.sql
@@ -10,3 +10,16 @@ CITY
 WHERE COUNTRYCODE='USA' and POPULATION > 120000;
 
 
+// Query all columns (attributes) for every row in the CITY table.
+
+SELECT *
+FROM CITY;
+
+
+// Query all columns for a city in CITY with the ID 1661.
+
+SELECT *
+FROM CITY
+WHERE ID='1661';
+
+
