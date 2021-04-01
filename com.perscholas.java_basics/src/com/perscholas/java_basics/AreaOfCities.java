4722aebc290d3699d7b45b6834c00f4c576318b5
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-04-01 09:48:22 -0400

    First Commit

---
+++ com.perscholas.java_basics/src/com/perscholas/java_basics/AreaOfCities.java
@@ -0,0 +1,31 @@
+package com.perscholas.java_basics;
+
+public class AreaOfCities {
+
+    /*
+  Suppose you need to estimate the area enclosed by four cities,
+  given the GPS locations (latitude and longitude) of these cities,
+  as shown in the following diagram. How would you write a program
+  to solve this problem?
+ */
+/*
+public class areaOfCities {
+    public static void main(String[] args) {
+        double atlantaLat = 33.7489954, atlantaLong = 84.3879824;
+        double charlotteLat = 35.2270869, charlotteLong = 80.8431267;
+        double savannahlat = 32.0835407, atlantaLong = 81.0998342;
+        double orlandolat = 28.5383355, orlandoLong = 81.3792365;
+
+        double atlantaCharlotteLat = charlotteLat - atlantaLat;
+        double atlantaCharlotteLong = atlantaLong - charlotteLong;
+
+        double atlantaOrlandoLat = atlantaLat - orlandolat;
+        double atlantaOrlandoLong = atlantaLong - orlandoLong;
+
+
+        System.out.println();
+    }
+}
+
+
+}
