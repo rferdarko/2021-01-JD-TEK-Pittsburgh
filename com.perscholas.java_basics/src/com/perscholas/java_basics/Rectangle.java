444cbf23d2c0717835acfef88974fb87d4e0d6fa
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-04-03 22:39:41 -0400

    first commit

---
+++ com.perscholas.java_basics/src/com/perscholas/java_basics/Rectangle.java
@@ -0,0 +1,9 @@
+package com.perscholas.java_basics;
+
+public class Rectangle {   // rectangle class
+    int height;  // integer variable for height
+    int width;   // integer variable for width
+
+    void printArea() {     // method to print area of rectangle
+        System.out.println(height * width); // prints height * width
+    }
