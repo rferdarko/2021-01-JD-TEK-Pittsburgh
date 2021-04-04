c1208df9d257dc941e1d3bd4310a8e5279c98cee
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-04-04 08:04:50 -0400

    seventh commit

--- com.perscholas.java_basics/src/com/perscholas/java_basics/Rectangle.java
+++ com.perscholas.java_basics/src/com/perscholas/java_basics/Rectangle.java
@@ -23,13 +23,17 @@ public class Rectangle {   // rectangle class
     public int getArea() {    // setter for area
         return height * width;  // return value
     }
+    public int getPerimeter() {  // method to return perimeter
+        return (height * 2) + (width * 2); // returns perimeter
+    }
 
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle(); // uses Rectangle Class to instantiate r1
-        r1.setHeight(5);  // setter method for height
+        r1.setHeight(7);  // setter method for height
         r1.setWidth(9);   // setter method for width
         System.out.println("Rectangle r1 height is "+ r1.getHeight()); // prints r1 height
         System.out.println("Rectangle r1 Width is " + r1.getWidth()); // prints r1 width
         System.out.println("The area of the rectangle is " + r1.getArea());  // prints r1 area
+        System.out.println("The perimeter of the rectangle is "+ r1.getPerimeter()); // Prints perimeter
     }
 }
