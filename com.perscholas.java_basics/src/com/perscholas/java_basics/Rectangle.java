5e8d4d04e3880bdd6f00aca3af593543c2c8a894
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-04-03 23:02:09 -0400

    fourth commit

--- com.perscholas.java_basics/src/com/perscholas/java_basics/Rectangle.java
+++ com.perscholas.java_basics/src/com/perscholas/java_basics/Rectangle.java
@@ -4,14 +4,20 @@ public class Rectangle {   // rectangle class
     int height;  // integer variable for height
     int width;   // integer variable for width
 
+    void setHeight(int h) {  // getter method or height
+        height = h;
+    }
+    void setWidth(int w) {   // getter method for width
+        width = w;
+    }
     void printArea() {     // method to print area of rectangle, no return value
         System.out.println("The are of the rectangle is: "+ height * width); // prints area of rectangle
     }
 
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle(); // uses Rectangle Class to instantiate r1
-        r1.height = 5;  // height for Rectangle r1
-        r1.width = 9;   // width for Rectangle r1
+        r1.setHeight(5);  // setter method for height
+        r1.setWidth(9);   // setter method for width
         r1.printArea(); // calls printArea method from Rectangle Class
     }
 }
