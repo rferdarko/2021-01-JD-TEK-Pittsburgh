80b69b250de25e0ba74bfe5f4dee60b2f8b251a8
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-04-05 13:33:56 -0400

    first commit

---
+++ com.perscholas.java_basics/src/com/perscholas/java_basics/LearningArrays.java
@@ -0,0 +1,11 @@
+package com.perscholas.java_basics;
+
+public class LearningArrays {
+    public static void main(String[] args) {
+        int[] t = new int[]{1,2,3,4,5};  // Another way to initialize an array
+        for (int i=0; i < t.length; i++) {
+            System.out.println(t[i]);
+        }
+
+    }
+}
