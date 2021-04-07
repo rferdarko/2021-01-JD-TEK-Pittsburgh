59b022494df8745d46067fa775d6e5be39c8c4e3
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-04-06 20:52:09 -0400

    second commmit

--- com.perscholas.java_basics/src/com/perscholas/students/Main.java
+++ com.perscholas.java_basics/src/com/perscholas/students/Main.java
@@ -6,8 +6,8 @@ public class Main {                              // Main Class
         student.setName("Joe Bloggs");           // student uses setter for Student Class setName
         student.setDateOfBirth("12/01/1901");    // student uses setter for Student Class setDateOfBirth
         student.hasStudentHousing(true);         // student used setter for Student Class hasStudentHousing
-        System.out.println(student.getName());   // Prints out student name
-        System.out.println(student.getDateOfBirth());  // Prints students date of birth
-        System.out.println(student.hasStudentHousing()); // prints students student housing status
+        System.out.println("Name: "+ student.getName());   // Prints out student name
+        System.out.println("DOB: "+ student.getDateOfBirth());  // Prints students date of birth
+        System.out.println("Has student housing: " +student.hasStudentHousing()); // prints students student housing status
     }
 }
