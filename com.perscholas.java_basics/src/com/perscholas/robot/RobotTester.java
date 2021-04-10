af07b5fd31f84994c45c1c9731610b9ba6a02ce0
rferdarko <65411244+rferdarko@users.noreply.github.com>
2021-04-10 18:31:27 -0400

    second commmit

--- com.perscholas.java_basics/src/com/perscholas/robot/RobotTester.java
+++ com.perscholas.java_basics/src/com/perscholas/robot/RobotTester.java
@@ -3,7 +3,7 @@ package com.perscholas.robot;
 public class RobotTester {
     public static void main(String[] args) {
 
-        Robot myRobo = new Robot();   // intantiated the Robot method by using new and made a call to its constructor
+        Robot myRobo = new Robot();   // instantiated the Robot method by using new and made a call to its constructor
         // Once we have the Robot, we can use the . operator to access its methods
         myRobo.travel();   // when we run this we get back the maxSpeed of the Robot
 
